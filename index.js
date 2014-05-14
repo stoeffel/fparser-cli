cecedb25be26746862bd20daf325b82bfe110793
Christoph Hermann <schtoeffel@gmail.com>
2014-05-14 16:01:37 +0200

    adds asking for other values

--- index.js
+++ index.js
@@ -2,30 +2,21 @@ var Formula = require('fparser'),
     inquander = require('inquander'),
     inquirer = require('inquirer'),
     _ = require('lodash'),
-    program = require('commander');
+    program = require('commander'),
+    result, formula, variables;
 
 program
     .version('0.0.1')
     .command('* <formula> [values]')
     .description('Parses a mathematical formula from a string')
     .action(function(expression, values) {
-        var formula = new Formula(expression),
-            variables = formula.getVariables(),
-            result;
+        formula = new Formula(expression);
+        variables = formula.getVariables();
         if (values) {
             result = formula.evaluate(getValuesObject(values));
             console.log('Result:', result);
         } else {
-            inquirer.prompt(_.map(variables, function(variable) {
-                return {
-                    type: 'input',
-                    name: variable,
-                    message: variable
-                };
-            }), function(answers) {
-                result = formula.evaluate(answers);
-                console.log('Result:', result);
-            });
+            askForValues();
         }
     });
 
@@ -34,8 +25,31 @@ inquander.parse(program, process.argv, {
     hidden: ['values']
 });
 
+function askForValues() {
+    inquirer.prompt(_.map(variables, function(variable) {
+        return {
+            type: 'input',
+            name: variable,
+            message: variable
+        };
+    }), function(answers) {
+        result = formula.evaluate(answers);
+        console.log('Result:', result);
+        inquirer.prompt([{
+            type: 'confirm',
+            name: 'again',
+            message: 'Would you like to calculate other values?'
+        }], function(answer) {
+            if (answer.again) {
+                askForValues();
+            }
+        });
+    });
+}
+
 function getValuesObject(values) {
-    var result = [], obj = {};
+    var result = [],
+        obj = {};
     values = values.split(',');
     _.each(values, function(value) {
         var split = value.split('='),
